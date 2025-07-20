41939675b3d05876fa529a18e6319f41f74992dd
Praciqx <vladislav.schangin@web.de>
2025-07-20 21:50:00 +0200

    Datum mitspeichern und die Werte wieder löschen nach dem speichern oder neu initialisieren

--- src/views/AddReminderComponent.vue
+++ src/views/AddReminderComponent.vue
@@ -17,26 +17,31 @@
 
 <script lang="ts">
 
-import { Preferences } from '@capacitor/preferences';
-import { IonButton,IonPage,IonHeader,IonContent,IonInput, IonToolbar, IonTitle } from '@ionic/vue';
+import { IonLabel, IonDatetime, IonButton,IonPage,IonHeader,IonContent,IonInput, IonToolbar, IonTitle } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { createReminder, saveReminder } from '@/utils/reminderHelpers';
 
 export default defineComponent({
-    components:{IonTitle,IonButton,IonInput,IonContent,IonPage,IonHeader,IonToolbar},
+    components:{IonLabel,IonDatetime,IonTitle,IonButton,IonInput,IonContent,IonPage,IonHeader,IonToolbar},
     data() {
         return {
             title:"",
             details:"",
+            toDate: new Date().toISOString(),
         };
     },
     methods:{
         async addReminder(){
             const title = this.title;
             const details = this.details;
-            const reminderObj = createReminder(title, details, '2025-01-01');
+            const toDate = this.toDate;
+            const reminderObj = createReminder(title, details, toDate);
             await saveReminder(reminderObj);
         },
+    },
+    created(){
+        this.title = "";
+        this.details = "";
     }
 });
 
