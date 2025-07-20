b02de793a79d4b991c73297665de578e5dcaab80
Praciqx <vladislav.schangin@web.de>
2025-07-20 19:12:59 +0200

    erste Implementierung der Home View und des Add Components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import HomePage from '../views/HomePage.vue';
+import AddReminderComponent from '../views/AddReminderComponent.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +12,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path:'/add',
+    name:'AddReminder',
+    component: AddReminderComponent,
   }
 ]
 
---
+++ src/views/AddReminderComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <ion-page>
+        <ion-header :translucent="true">
+        <ion-toolbar>
+            <ion-title>Reminder hinzufügen</ion-title>
+        </ion-toolbar>
+        </ion-header>
+    </ion-page>
+    <ion-content :fullscreen="true">
+        <ion-input label="Titel" label-placement="floating" placeholder="Titel eingeben">
+        </ion-input>
+        <ion-input label="Details" label-placement="floating" placeholder="Beschreibung eingeben">
+        </ion-input>
+        <ion-button expand="block" shape="round">Speichern</ion-button>
+    </ion-content>
+</template>
+
+<script lang="ts">
+
+import { IonButton,IonPage,IonHeader,IonContent,IonInput, IonToolbar, IonTitle } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    components:{IonTitle,IonButton,IonInput,IonContent,IonPage,IonHeader,IonToolbar},
+    data() {
+        return {
+            text:"Das ist ein Component"
+        };
+    },
+});
+
+
+
+</script>
