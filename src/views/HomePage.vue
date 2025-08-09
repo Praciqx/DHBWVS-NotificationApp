a666dfd657ea135bce79f8cc5ce2688d0d33abd1
Praciqx <vladislav.schangin@web.de>
2025-08-09 14:31:03 +0200

    neues Design

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,29 +2,33 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Erinnerung</ion-title>
+        <ion-title>Erinnerungen</ion-title>
       </ion-toolbar>
     </ion-header>
-
     <ion-content :fullscreen="true">
-      <ion-card v-for="r in reminders" :key="r.id" v-on:click="editReminder(r)">
-        <ion-card-header>
-          <ion-card-title>{{ r.title }}</ion-card-title>
-          <ion-card-subtitle>
-            Datum:
-            {{ new Date(r.date).toLocaleDateString('de-DE',{
-              day:'2-digit',
-              month:'2-digit',
-              year:'numeric',
-              hour:'2-digit',
-              minute:'2-digit'
-            })}}
-          </ion-card-subtitle>
-        </ion-card-header>
-        <ion-card-content>
-          {{r.details}}
-        </ion-card-content>
-      </ion-card>
+      <ion-list v-for="r in reminders" :key="r.id" lines="none">
+        <ion-item>
+           <div style="text-align:center; width:48px;">
+            <div style="font-size:20px; font-weight:bold;">
+              {{ new Date(r.date).getDate() }}
+            </div>
+            <div style="font-size:14px; color:gray;">
+              {{ new Date(r.date).toLocaleDateString('de-DE', { weekday: 'short',month:'short' }) }}
+            </div>
+          </div>
+          <ion-card style="width: 100%;" 
+            :style="{backgroundColor:'rgba(62, 201, 238, 0.65)',color:'#1a1a1a'}" 
+            @click="editReminder(r)">
+            <ion-card-header>
+              <ion-card-title>{{ r.title }}</ion-card-title>
+            </ion-card-header>
+            <ion-card-content>
+              {{ new Date(r.date).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' }) }}
+              <span v-if="r.details"> – {{ r.details }}</span>
+            </ion-card-content>
+          </ion-card>
+        </ion-item>
+      </ion-list>
       <ion-fab slot="fixed" vertical="bottom" horizontal="end">
         <ion-fab-button title="addReminder" router-link="/add" router-direction="forward">
           <ion-icon :icon="add" title="addReminderIcon"></ion-icon>
@@ -56,12 +60,18 @@
        },
       editReminder(r:any){
         this.router.push({ name: 'EditReminder', params: { id: r.id } })
+      },
+      async load(){
+        this.reminders = await getCurrentReminder();
       }
     },
     data(){
       return {
         reminders:[] as any[],
       }
+    },  
+    ionViewWillEnter() {
+      this.load();
     },
     async created(){
       this.reminders = await getCurrentReminder();
