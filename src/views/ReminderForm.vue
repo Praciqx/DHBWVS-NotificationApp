77f068839f34591d318e46701a768c5d1c584aaa
Praciqx <vladislav.schangin@web.de>
2025-08-11 14:41:48 +0200

    Abfrage ob Reminder gelöscht werden soll

--- src/views/ReminderForm.vue
+++ src/views/ReminderForm.vue
@@ -1,11 +1,4 @@
 <template>
-    <ion-popover trigger="reminder-menu" trigger-action="hover">
-        <ion-content>
-            <ion-list lines="none">
-                <ion-item :button="true" :detail="false" @click="deleteReminder">Löschen</ion-item>
-            </ion-list>
-        </ion-content>
-    </ion-popover>
     <ion-page>
         <ion-header :translucent="true">
         <ion-toolbar>
@@ -13,10 +6,7 @@
                 <ion-back-button default-href="/home" />
             </ion-buttons>
             <ion-title>{{isEditMode ? "Erinnerung bearbeiten" : "Erinnerung hinzufügen"}}</ion-title>
-            <ion-buttons slot="primary">
-                <ion-button id="reminder-menu">
-                    <ion-icon slot="icon-only" :icon="ellipsisVertical"></ion-icon>
-                </ion-button>
+                <ion-buttons slot="primary">
             </ion-buttons>
         </ion-toolbar>
         </ion-header>
@@ -27,11 +17,11 @@
                  :class="{ 'ion-invalid': showErrors && title.trim() === '', 'ion-touched': showErrors }"
                 :errorText="title.trim() == '' && showErrors ? 'Titel ist erforderlich' : ''"
                 >
-                <ion-icon slot="start" :icon="pencil" aria-hidden="true"></ion-icon>
+                <ion-icon slot="start" :icon="pencil"></ion-icon>
             </ion-input>
             <ion-input class="ion-margin-top ion-margin-bottom" v-model="details"
                 placeholder="Beschreibung hinzufügen">
-                  <ion-icon slot="start" :icon="listOutline" aria-hidden="true"></ion-icon>
+                  <ion-icon slot="start" :icon="listOutline"></ion-icon>
             </ion-input>
             <ion-label>Uhrzeit</ion-label>
              <ion-datetime v-model="toDate"
@@ -40,13 +30,21 @@
                 <span slot="time-label">Uhrzeit</span>
             </ion-datetime>
             <ion-button expand="block" @click="addReminder" shape="round">Speichern</ion-button>
+            <ion-button v-if="isEditMode" expand="block" color="danger" id="deleteAlert" shape="round">Löschen</ion-button>
         </ion-content>
+         <ion-alert
+            trigger="deleteAlert"
+            header="Löschen"
+            message="Möchten Sie diese Erinnerung löschen?"
+            :buttons="alertButtons"
+            >
+        </ion-alert>
     </ion-page>
 </template>
 
 <script lang="ts">
 
-import { IonPopover,IonItem,IonList,IonBackButton,IonButtons,IonIcon,IonLabel, IonDatetime, IonButton,IonPage,IonHeader,IonContent,IonInput, IonToolbar, IonTitle } from '@ionic/vue';
+import { IonAlert,IonPopover,IonItem,IonList,IonBackButton,IonButtons,IonIcon,IonLabel, IonDatetime, IonButton,IonPage,IonHeader,IonContent,IonInput, IonToolbar, IonTitle } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import {listOutline,pencil,ellipsisVertical} from 'ionicons/icons'
 import { createReminder, saveReminder,getReminderById, editReminder,deleteReminderById } from '@/utils/reminderHelpers';
@@ -59,7 +57,7 @@ export default defineComponent({
         const route = useRoute();
         return {listOutline,pencil, route,ellipsisVertical};
     },
-    components:{Preferences,IonPopover, IonList, IonItem,IonBackButton,IonButtons,IonIcon,IonLabel,IonDatetime,IonTitle,IonButton,IonInput,IonContent,IonPage,IonHeader,IonToolbar},
+    components:{IonAlert,Preferences,IonPopover, IonList, IonItem,IonBackButton,IonButtons,IonIcon,IonLabel,IonDatetime,IonTitle,IonButton,IonInput,IonContent,IonPage,IonHeader,IonToolbar},
     data() {
         return {
             id: null as string | null,
@@ -68,6 +66,10 @@ export default defineComponent({
             details:"",
             toDate: new Date().toISOString(),
             showErrors:false,
+            alertButtons:[
+                {text:'Abbrechen',role:'cancel'},
+                {text:'Ja',role:'confirm',handler:()=>this.deleteReminder()}
+            ]
         };
     },
     created(){
@@ -107,7 +109,7 @@ export default defineComponent({
                 this.details = rem.details;
                 this.toDate = rem.date;
             }
-        }
+        },
     },
 });
 
